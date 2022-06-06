6e370741eaab590dc87c288fb16eeca6d14f910b
ihsan.satriawan <ihsan.satriawan@BA-00021650.local>
2022-06-06 13:58:23 +0700

    Add 2 screen for data and result screen bmi

---
+++ lib/bmi_data_screen.dart
@@ -0,0 +1,49 @@
+import 'package:bmi_apps/bmi_result_screen.dart';
+import 'package:flutter/material.dart';
+
+class BmiDataScreen extends StatefulWidget {
+  const BmiDataScreen({Key? key}) : super(key: key);
+
+  @override
+  State<BmiDataScreen> createState() => _BmiDataScreenState();
+}
+
+class _BmiDataScreenState extends State<BmiDataScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BMI Calculator"),
+      ),
+      body: Column(
+        children: [
+          Expanded(child: Container(
+            color: Colors.red,
+          )),
+          Expanded(child: Container(
+            color: Colors.yellow,
+          )),
+          Expanded(child: Container(
+            color: Colors.green,
+          )),
+          GestureDetector(
+            onTap: (){
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: ((context) {
+                    return BmiResultScreen();
+                  }),
+                ),
+              );
+            },
+            child: Container(
+              height: 60,
+              color: Colors.blue,
+              child: Center(child: Text("Hitung BMI")),
+            ),
+          )
+        ],
+      ),      
+    );
+  }
+}
