6dced24c8fcc35db778daaa66e40ac3329e6cc59
ihsan.satriawan <ihsan.satriawan@BA-00021650.local>
2022-06-08 05:03:13 +0700

    Add style for adjust height section

--- lib/constants/constant.dart
+++ lib/constants/constant.dart
@@ -5,3 +5,7 @@ TextStyle genderTextStyle = const TextStyle(
   fontSize: 18, 
   color: Color(0xff8d8e98)
 );
+TextStyle labelTextStyle = const TextStyle(
+  fontSize: 18,
+  color: Colors.white
+);
